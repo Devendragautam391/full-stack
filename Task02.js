ca382b7e195b4ab7fb1d1c12d2f8d40f81b964b6
Devendra Raj Gautam <devengautam43@gmail.com>
2026-04-20 14:27:25 +0300

    Delete Task02.js

--- Task02.js
+++
@@ -1,6 +0,0 @@
-const inches = parseFloat(prompt('Enter length in inches:'))
-while (inches > 0) {
-  centimeters = inches*2.54
-  alert(`${inches.toFixed(2)} inches = ${centimeters.toFixed(2)} centimeters`)
-  break;
-}
